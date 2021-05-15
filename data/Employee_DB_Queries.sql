99f7601df72bc0a13bcecd1d8365dea8327c70c5
Cleveland <eclevela@richmond.edu>
2021-05-15 01:54:42 -0400

    adding queries 2 & 3

--- data/Employee_DB_Queries.sql
+++ data/Employee_DB_Queries.sql
@@ -3,3 +3,13 @@
 select employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary
 from employees inner join salaries on employees.emp_no = salaries.emp_no;
 
+-- query question #2
+
+select employees.first_name, employees.last_name, employees.hire_date
+from employees where extract(year from "hire_date") = 1986; 
+
+-- query question #3
+
+select employees.last_name, employees.first_name, employees.emp_no, dept_manager.dept_no, departments.dept_name
+from employees inner join dept_manager on employees.emp_no = dept_manager.emp_no
+	inner join departments on dept_manager.dept_no = departments.dept_no;
