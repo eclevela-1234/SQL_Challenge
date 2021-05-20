d4d9c4debddaa5f01fed2778275b4a9cd618164b
Cleveland <eclevela@richmond.edu>
2021-05-19 23:20:43 -0400

    updating README

---
+++ README.md
@@ -0,0 +1,11 @@
+![PG logo poached from google](PG_Logo.png "https://images.app.goo.gl/EexjRZV3xohyTSNAA")
+
+# **SQL Challenge**
+---
+## Just a few queries on archived employment data from Pewlett Hackard using PostgreSQL
+###  1. First I brought in the data and created an Entity Relationship Diagram using PG Admin 
+####  - I made the key connections using the software then generated the schema script
+###  - 
+
+![This was super easy!](Employee_DB_ERD.pgerd.png)
+
--- data/Employee_DB_Queries.sql
+++ data/Employee_DB_Queries.sql
@@ -1,46 +1,54 @@
 -- query question #1
 
+create view query_1 as  
 select employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary
 from employees inner join salaries on employees.emp_no = salaries.emp_no;
 
 -- query question #2
 
+create view query_2 as  
 select employees.first_name, employees.last_name, employees.hire_date
 from employees where extract(year from "hire_date") = 1986; 
 
 -- query question #3
 
+create view query_3 as  
 select employees.last_name, employees.first_name, employees.emp_no, dept_manager.dept_no, departments.dept_name
 from employees inner join dept_manager on employees.emp_no = dept_manager.emp_no
 	inner join departments on dept_manager.dept_no = departments.dept_no;
 	
 -- query question #4
 
+create view query_4 as  
 select employees.last_name, employees.first_name, employees.emp_no, departments.dept_name
 from employees inner join dept_emp on employees.emp_no = dept_emp.emp_no
 	inner join departments on dept_emp.dept_no = departments.dept_no;
 
 -- query question #5
 
+create view query_5 as  
 select employees.first_name, employees.last_name, employees.sex
 from employees where employees.first_name = 'Hercules' and employees.last_name like 'B%';
 
 -- query question #6
 
+create view query_6 as  
 select employees.emp_no, employees.last_name, employees.first_name,  departments.dept_name
 from employees inner join dept_emp on employees.emp_no = dept_emp.emp_no
 	inner join departments on dept_emp.dept_no = departments.dept_no
-where departments.dept_name = 'Sales'
+where departments.dept_name = 'Sales';
 	
 -- query question #7
 
+create view query_7 as  
 select employees.emp_no, employees.last_name, employees.first_name,  departments.dept_name
 from employees inner join dept_emp on employees.emp_no = dept_emp.emp_no
 	inner join departments on dept_emp.dept_no = departments.dept_no
-where departments.dept_name = 'Sales' or departments.dept_name = 'Development'
+where departments.dept_name = 'Sales' or departments.dept_name = 'Development';
 
 -- query question #8
 
+create view query_8 as  
 select employees.last_name, count(employees.last_name)
 from employees
 group by employees.last_name
